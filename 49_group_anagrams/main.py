2b9870687b0399588247871941c47eb611c5e4e2
Damianos Vonapartis <damianosvon@gmail.com>
2025-10-17 18:32:51 +0300

    Add improved solution to 49 and format file.

--- 49_group_anagrams/main.py
+++ 49_group_anagrams/main.py
@@ -1,25 +1,35 @@
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        words = ["".join(sorted(word)) for word in strs]
-        hashes = list(set(words))
-        strs_words = sorted(zip(strs, words), key=lambda x: x[1])
+        groups = defaultdict(list)
+        for s in strs:
+            key = "".join(sorted(s))
+            groups[key].append(s)
+        return list(groups.values())
 
-        groups: list[list[str]] = []
-        group: list[str] = []
-        for hash in hashes:
-            group = []
-            for strs_word in strs_words:
-                if hash == strs_word[1]:
-                    group.append(strs_word[0])
-            groups.append(group)
-        return groups
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         words = ["".join(sorted(word)) for word in strs]
+#         hashes = list(set(words))
+#         strs_words = sorted(zip(strs, words), key=lambda x: x[1])
+
+#         groups: list[list[str]] = []
+#         group: list[str] = []
+#         for hash in hashes:
+#             group = []
+#             for strs_word in strs_words:
+#                 if hash == strs_word[1]:
+#                     group.append(strs_word[0])
+#             groups.append(group)
+#         return groups
 
 
 def main() -> None:
-    r1 = [["bat"],["nat","tan"],["ate","eat","tea"]] == Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+    r1 = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]] == Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
     r2 = [[""]] == Solution().groupAnagrams([""])
     r3 = [["a"]] == Solution().groupAnagrams(["a"])
     print(r1)

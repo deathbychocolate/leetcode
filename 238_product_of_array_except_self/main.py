b8f9250c5420b1fb4e15ab02702d155511d841d1
Damianos Vonapartis <damianosvon@gmail.com>
2025-10-17 23:12:26 +0300

    Add improved solution to 238.

--- 238_product_of_array_except_self/main.py
+++ 238_product_of_array_except_self/main.py
@@ -3,19 +3,34 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        products: list[int] = []
-        n: int = 0
-        while n < len(nums):
-            products.append(self.product(nums.copy(), n))
-            n += 1
+        nums_len: int = len(nums)
+        products: list[int] = [1] * nums_len
+        left_product = 1
+        right_product = 1
+        for i in range(nums_len):  # left product
+            products[i] = left_product
+            left_product *= nums[i]
+        for j in range(nums_len - 1, -1, -1):  # right product
+            products[j] *= right_product
+            right_product *= nums[j]
         return products
 
-    def product(self, nums: list[int], index: int) -> int:
-        del nums[index]
-        product: int = 1
-        for item in nums:
-            product *= item
-        return product
+
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         products: list[int] = []
+#         n: int = 0
+#         while n < len(nums):
+#             products.append(self.product(nums.copy(), n))
+#             n += 1
+#         return products
+
+#     def product(self, nums: list[int], index: int) -> int:
+#         del nums[index]
+#         product: int = 1
+#         for item in nums:
+#             product *= item
+#         return product
 
 
 def main() -> None:

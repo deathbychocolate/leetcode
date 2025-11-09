12baab55cb0ed6b38cb86a9f278221031573899e
Damianos Vonapartis <damianosvon@gmail.com>
2025-11-09 21:00:45 +0200

    Add optimal solution and fix tests.

--- 347_top_k_elements/main.py
+++ 347_top_k_elements/main.py
@@ -1,27 +1,30 @@
 from typing import List
+from collections import Counter
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d: dict[int, int] = {}
+        return [num for num, _ in Counter(nums).most_common(k)]
 
-        # count elements
-        for num in nums:
-            if not d.get(num):
-                d[num] = 1
-            else:
-                d[num] += 1
+        # d: dict[int, int] = {}
 
-        # sort by frequency
-        d = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+        # # count elements
+        # for num in nums:
+        #     if not d.get(num):
+        #         d[num] = 1
+        #     else:
+        #         d[num] += 1
 
-        return list(d.keys())[:k]
+        # # sort by frequency
+        # d = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
+
+        # return list(d.keys())[:k]
 
 
 def main() -> None:
-    r1: bool = [] == Solution().topKFrequent([1,1,1,2,2,3], k=2)
-    r2: bool = [] == Solution().topKFrequent([1], k=1)
-    r3: bool = [] == Solution().topKFrequent([1,2,1,2,1,2,3,1,3,2], k=2)
+    r1: bool = [1, 2] == Solution().topKFrequent([1, 1, 1, 2, 2, 3], k=2)
+    r2: bool = [1] == Solution().topKFrequent([1], k=1)
+    r3: bool = [1, 2] == Solution().topKFrequent([1, 2, 1, 2, 1, 2, 3, 1, 3, 2], k=2)
     print(r1)
     print(r2)
     print(r3)
